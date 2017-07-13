8fb34301a57c5cb643b7b0faf21d63deb6343103
Mateusz Gbiorczyk <mateusz.gbiorczyk@crafton.pl>
2017-07-13 13:12:57 +0200

    Header login

--- js/class/Core.js
+++ js/class/Core.js
@@ -8,19 +8,19 @@ Core.prototype = {
 	},
 
 	run: function(){
-		document._910ths.grid = new Grid( document.getElementById('homeGrid') );
-		document._910ths.showDelayed = new ShowDelayed();
-		document._910ths.multiHover = new MultiHover();
-		document._910ths.scrollTo = new ScrollTo();
-		document._910ths.EventMap = new EventMap();
-		document._910ths.video = new Video();
-		document._910ths.offersFilter = new OffersFilter();
+		// document._910ths.grid = new Grid( document.getElementById('homeGrid') );
+		// document._910ths.showDelayed = new ShowDelayed();
+		// document._910ths.multiHover = new MultiHover();
+		// document._910ths.scrollTo = new ScrollTo();
+		// document._910ths.EventMap = new EventMap();
+		// document._910ths.video = new Video();
+		// document._910ths.offersFilter = new OffersFilter();
 
-		document._910ths.popupVideo   = new PopupVideo();
-		document._910ths.popupYouTube = new PopupYouTube();
-		document._910ths.zendesk      = new Zendesk();
-		document._910ths.banner       = new Banner();
-		document._910ths.offerBox     = new OfferBox();
+		// document._910ths.popupVideo   = new PopupVideo();
+		// document._910ths.popupYouTube = new PopupYouTube();
+		// document._910ths.zendesk      = new Zendesk();
+		// document._910ths.banner       = new Banner();
+		// document._910ths.offerBox     = new OfferBox();
 	}
 
 };
